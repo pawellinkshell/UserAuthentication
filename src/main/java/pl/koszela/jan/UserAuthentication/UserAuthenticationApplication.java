db1626b39b9b0f51760fce15bc8944be42ffbf7f
Jan Koszela <jan.koszela@hicron.com>
2017-08-24 13:44:43 +0200

    Add SpringBoot files

---
+++ src/main/java/pl/koszela/jan/UserAuthentication/UserAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package pl.koszela.jan.UserAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationApplication.class, args);
+	}
+}
