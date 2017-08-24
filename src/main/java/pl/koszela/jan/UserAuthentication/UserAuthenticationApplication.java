3fd435f16396cb7fa0ebc249153e91d26258c804
Jan Koszela <jan.koszela@hicron.com>
2017-08-24 14:46:53 +0200

    Update pom.xml and SpringBoot Runner

--- src/main/java/pl/koszela/jan/UserAuthentication/UserAuthenticationApplication.java
+++ src/main/java/pl/koszela/jan/UserAuthentication/UserAuthenticationApplication.java
@@ -1,12 +1,22 @@
 package pl.koszela.jan.UserAuthentication;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class UserAuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserAuthenticationApplication.class, args);
+		ApplicationContext context = SpringApplication.run(UserAuthenticationApplication.class, args);
+
+		System.out.println("Inspect the beans provided by Spring Boot: ");
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String bean : beanNames) {
+			System.out.println(bean);
+		}
+
 	}
 }
