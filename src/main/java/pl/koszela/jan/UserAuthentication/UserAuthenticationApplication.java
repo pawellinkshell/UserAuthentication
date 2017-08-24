3f9777ecf8d4117a272f70add753ef995fecf600
Jan Koszela <jan.koszela@hicron.com>
2017-08-24 16:33:53 +0200

    Extend main application by Servlet

--- src/main/java/pl/koszela/jan/UserAuthentication/UserAuthenticationApplication.java
+++ src/main/java/pl/koszela/jan/UserAuthentication/UserAuthenticationApplication.java
@@ -3,20 +3,27 @@ package pl.koszela.jan.UserAuthentication;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class UserAuthenticationApplication {
+public class UserAuthenticationApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(UserAuthenticationApplication.class, args);
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(UserAuthenticationApplication.class);
+  }
 
-		System.out.println("Inspect the beans provided by Spring Boot: ");
-		String[] beanNames = context.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String bean : beanNames) {
-			System.out.println(bean);
-		}
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(UserAuthenticationApplication.class, args);
 
-	}
+    System.out.println("Inspect the beans provided by Spring Boot: ");
+    String[] beanNames = context.getBeanDefinitionNames();
+    Arrays.sort(beanNames);
+    for (String bean : beanNames) {
+      System.out.println(bean);
+    }
+
+  }
 }
